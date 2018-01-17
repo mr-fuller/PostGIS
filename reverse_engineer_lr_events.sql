5df9caa7672206e231e3eaecd273a6f585219b31
mr-fuller <mrfuller460@gmail.com>
2018-01-17 10:19:57 -0500

    update reverse_engineer_lr_events.sql
    add lines to establish from and to measures of lines from points

--- reverse_engineer_lr_events.sql
+++ reverse_engineer_lr_events.sql
@@ -1,4 +1,7 @@
 -- All the SQL below is in aid of creating the new event table
+-- from an input point geometry table
+drop table los_2018_def_events_oh;
+
 -- The transform statements are needed because the road layer was in EPSG: 3857
 CREATE TABLE los_2018_def_events AS
 -- We first need to get a candidate set of maybe-closest
@@ -12,10 +15,23 @@ SELECT
   streets.ctl_end as streets_ctl_end,
   locations.geom AS locations_geom,
   locations.gid AS locations_gid,
+  locations.los as locations_los,
+  locations.ix_leg as locations_ix_leg,
+  locations.direction as locations_direction,
+  locations.on_road as locations_on_road,
+  --subways.i
+  locations.juris as locations_juris,
+  locations."2013_loc" as locations_2013_loc,
+  locations.on_fc as locations_on_fc,
+  locations.ix_fc as locations_ix_fc,
+  locations.on_fedaid as locations_fedaid,
+  locations.assume_rd as locations_assume_rd,
   ST_Distance(ST_transform(streets.geom,3734), locations.geom) AS distance
 FROM road_inventory_3857 streets
   JOIN los_2018_def locations
   ON ST_DWithin(st_transform(streets.geom,3734), locations.geom, 75)
+  WHERE (streets.county_cd = 'LUC' or streets.county_cd = 'WOO') 
+  AND (locations.juris <> 'Whiteford Township' AND locations.juris <> 'Bedford Township')  
 ORDER BY locations_gid, distance ASC
 )
 -- We use the 'distinct on' PostgreSQL feature to get the first
@@ -29,26 +45,38 @@ SELECT
   streets_gid,
   streets_nlfid
   ST_LineLocatePoint(streets_geom, locations_geom) AS measure,
+  
   streets_ctl_begin,
   streets_ctl_end,
   distance
+  locations_los,
+  locations_ix_leg,
+  locations_direction,
+  locations_on_road,
+  locations_juris,
+  locations_2013_loc,
+  locations_on_fc,
+  locations_ix_fc,
+  locations_fedaid,
+  locations_assume_rd
 FROM ordered_nearest;
 
 -- Then use the begin and end log points with the measure
 -- to calculate the log point of the event.
 
-alter table los_2018_def_events add column ctl_pt float, bmp float, emp float
-update table set ctl_pt = streets_ctl_begin + measure * (streets_ctl_end - streets_ctl_begin);
-
--- turn points into really small lines, first the case where ctl_pt isn't on a boundary
-update table set bmp = ctl_pt;
-update table set emp = bmp + 0.001;
---case where point is on the start of the linear reference segment
-update table set bmp = ctl_pt + 0.001 where ctl_pt = streets_ctl_begin;
-update table set emp = bmp + 0.001 where ctl_pt = streets_ctl_begin;
---case where point is on the end of the linear reference segment
-update table set emp = ctl_pt - 0.001 where ctl_pt = streets_ctl_end;
-update table set bmp = emp + 0.001 where ctl_pt = streets_ctl_end;
-
 -- Primary keys are useful for visualization softwares
-ALTER TABLE los_2018_def_events ADD PRIMARY KEY (locations_gid);
+ALTER TABLE los_2018_def_events_oh ADD PRIMARY KEY (subways_gid);
+alter table los_2018_def_events_oh add column meas double precision, add column fmeas double precision, add column tmeas double precision;
+update los_2018_def_events_oh set meas = ROUND (cast((streets_ctl_begin + (streets_ctl_end - streets_ctl_begin)*measure) as numeric),3);
+--this should work for cases where meas = streets_ctl_end or meas <> streets_ctl_begin or meas <> streets_ctl_end
+--0.001 is too short, as it created some null geometries; 0.004 was the minimum that created non-null geometries for all rows in table
+update los_2018_def_events_oh set fmeas = meas where streets_ctl_end - meas > 0.004;
+update los_2018_def_events_oh set tmeas = meas + 0.004 where streets_ctl_end - meas > 0.004;
+
+update los_2018_def_events_oh set fmeas = meas - 0.004 where meas - streets_ctl_begin > 0.004;
+update los_2018_def_events_oh set tmeas = meas  where meas - streets_ctl_begin > 0.004;
+
+update los_2018_def_events_oh set fmeas = streets_ctl_begin where streets_ctl_end - streets_ctl_begin <= 0.004;
+update los_2018_def_events_oh set tmeas = streets_ctl_end where streets_ctl_end - streets_ctl_begin <= 0.004;
+
+
